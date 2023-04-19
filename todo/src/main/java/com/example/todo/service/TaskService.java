425562927c838cc57a832ddaaf1a3e6458fb65d3
leandro.i.farias <leandro.inacio.farias@gmail.com>
2023-04-19 17:51:04 -0300

    Ajuste no metodo create e findAll da classe TaskService

--- todo/src/main/java/com/example/todo/service/TaskService.java
+++ todo/src/main/java/com/example/todo/service/TaskService.java
@@ -2,20 +2,28 @@ package com.example.todo.service;
 
 
 import com.example.todo.model.Task;
+import com.example.todo.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class TaskService {
 
-    public Task create() {
-        return new Task();
+    @Autowired
+    private TaskRepository repository;
+
+    public Task create(Task task) {
+        task.setCreatedAt(LocalDateTime.now());
+        task.setIsCompleted(false);
+        task.setIsCompleted(null);
+        return this.repository.save(task);
     }
 
     public List<Task> findAll() {
-        return new ArrayList<Task>();
+        return repository.findAll();
     }
 
     public Task findById(Long id) {
