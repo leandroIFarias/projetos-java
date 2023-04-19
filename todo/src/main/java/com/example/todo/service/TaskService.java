e6f2ba67c27b295ac6d6280656255cf172184f69
leandro.i.farias <leandro.inacio.farias@gmail.com>
2023-04-19 17:26:09 -0300

    Criacao do esqueleto da classe TaskService

--- todo/src/main/java/com/example/todo/service/TaskService.java
+++ todo/src/main/java/com/example/todo/service/TaskService.java
@@ -1,8 +1,32 @@
 package com.example.todo.service;
 
 
+import com.example.todo.model.Task;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TaskService {
+
+    public Task create() {
+        return new Task();
+    }
+
+    public List<Task> findAll() {
+        return new ArrayList<Task>();
+    }
+
+    public Task findById(Long id) {
+        return new Task();
+    }
+
+    public void delete(Long id) {
+
+    }
+
+    public Task update(Task task) {
+        return new Task();
+    }
 }
